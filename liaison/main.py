dbd8ebb32482910506f71ee5791a04b9ae0ff810
Cameron Ruatta <cameron@ruatta.com>
2015-12-14 23:12:23 -0800

    Adding proper documentation to the new functions. Fixing a bug where consul_agent was passed when consul_config should have been.

--- liaison/main.py
+++ liaison/main.py
@@ -6,16 +6,20 @@ import multiprocessing
 import time
 
 
-def create_pool(liason_config):
+def create_pool(liaison_config):
     """
 
-    :param size:
-    :return:
+    :param liaison_config: LiaisonConfig object
+    :type liaison_config: LiaisonConfig
+
+    :return pool, pool_size: A multiprocessing pool and the size
+    the pool should be.
+    :rtype: multiprocessing.Pool, int
     """
-    if liason_config.pool_size is None:
+    if liaison_config.pool_size is None:
         pool_size = multiprocessing.cpu_count()
     else:
-        pool_size = liason_config.pool_size
+        pool_size = liaison_config.pool_size
 
     pool = multiprocessing.Pool(pool_size)
 
@@ -25,10 +29,15 @@ def create_pool(liason_config):
 def create_check_service_jobs(services, consul_config, sink_config):
     """
 
-    :param services:
-    :param consul_config:
-    :param sink_config:
-    :return:
+    :param services: A dict of services and tags
+    :type services: dict
+    :param consul_config: Consul Config
+    :type consul_config: ConsulConfig
+    :param sink_config: Sink Config
+    :type sink_config: SinkConfig
+
+    :return: A list of check service jobs
+    :rtype: dict
     """
     check_service_jobs = list()
     for name, tags in services.iteritems():
@@ -45,8 +54,11 @@ def create_check_service_jobs(services, consul_config, sink_config):
 def get_dc(consul_agent):
     """
 
-    :param consul_agent:
-    :return:
+    :param consul_agent: A Consul object
+    :type consul_agent: consul.Consul
+
+    :return: The datacenter of the agent
+    :rtype: str
     """
     self = consul_agent.agent.self()
     dc = self['Config']['Datacenter']
@@ -56,10 +68,17 @@ def get_dc(consul_agent):
 def get_health_service(consul_agent, service, tag=None):
     """
 
-    :param consul_agent:
-    :param service:
-    :param tag:
-    :return:
+    :param consul_agent: A Consul object
+    :type consul_agent: consul.Consul
+    :param service: The name of the consul servie
+    :type service: str
+    :param tag: A tag for the service
+    :type tag: str|None
+
+    :return: A dictionary of representation of the result of
+    a query to /v1/health/service/<service>
+    :rtype: dict
+
     """
     if tag:
         _, health_service = consul_agent.health.service(service, tag=tag)
@@ -72,8 +91,13 @@ def get_health_service(consul_agent, service, tag=None):
 def get_node_status(consul_health_service):
     """
 
-    :param consul_health_service:
-    :return:
+    :param consul_health_service: A dictionary of representation
+    of the result of a query to /v1/health/service/<service>
+    :type consul_health_service: dict
+
+    :rtype: int, int
+    :return: number of nodes without critical checks,
+        number of nodes with critical checks
     """
     nodes = dict()
     ok = 0
@@ -163,7 +187,7 @@ def loop(liaison_config):
     index, services = consul_agent.catalog.services()
 
     check_service_jobs = create_check_service_jobs(services,
-                                                   consul_agent,
+                                                   consul_config,
                                                    sink_config)
 
     pool, pool_size = create_pool(liaison_config)
@@ -171,10 +195,11 @@ def loop(liaison_config):
     while len(check_service_jobs) > 0:
         if len(check_service_jobs) >= pool_size:
             pool.map(check_service, [check_service_jobs.pop()
-                     for _ in xrange(pool_size)])
+                                     for _ in xrange(pool_size)])
         else:
             pool.map(check_service, [check_service_jobs.pop()
-                     for _ in xrange(len(check_service_jobs))])
+                                     for _ in xrange(len(check_service_jobs))])
+
         time.sleep(liaison_config.sleep)
 
     pool.close()
