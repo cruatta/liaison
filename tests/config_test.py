e5f0b2cf423d5d9469ee18b3e4e5acb05cc6eec0
Cameron Ruatta <cameron.ruatta@creditkarma.com>
2015-12-28 14:14:25 -0800

    0.4.0_rc1 Python3 Exceptions have no attribute message so we just print them

--- tests/config_test.py
+++ tests/config_test.py
@@ -80,4 +80,4 @@ class ConfigTests(unittest.TestCase):
         try:
             load_config('')
         except Exception as e:
-            self.assertEqual(e.message, 'Invalid Sink configuration.')
+            self.assertEqual(e, 'Invalid Sink configuration.')
